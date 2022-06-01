bd284c2dc9c6afb06bc9b85855865ab1bf6db759
divine <divineobi07@gmail.com>
2022-06-01 23:29:27 +0100

    js file

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const page = document.querySelectorAll(
+      ".chat_app__illustration__mobile__inner__message > *"
+    );
+
+   page.forEach((el, i) => {
+       el.style.transform = 'translateY(0)'
+       el.style.transition = `a ${i}s`;
+       el.style.opacity = 1
+   })
+})
